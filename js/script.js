5740501a8b8cb53a17ca5603d15bb5a8aca80167
Andrey <examp@mail.com>
2021-06-04 02:03:10 +0300

    food

--- js/script.js
+++ js/script.js
@@ -433,12 +433,16 @@ window.addEventListener('DOMContentLoaded', () => {
 		dots[slideIndex - 1].style.opacity = 1;
 	}
 
+	function changeString(str) {
+		return +str.replace(/\D/g, '');
+	}
+
 
 	next.addEventListener('click', () => {
-		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+		if (offset == changeString(width) * (slides.length - 1)) {
 			offset = 0;
 		} else {
-			offset += +width.slice(0, width.length - 2);
+			offset += changeString(width);
 		}
 
 		slidesField.style.transform = `translateX(-${offset}px)`;
@@ -449,7 +453,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			slideIndex++;
 		}
 
-
 		currentSlide();
 		dotsOpacity();
 	});
@@ -457,9 +460,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	prev.addEventListener('click', () => {
 		if (offset == 0) {
-			offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+			offset = changeString(width) * (slides.length - 1);
 		} else {
-			offset -= +width.slice(0, width.length - 2);
+			offset -= changeString(width);
 		}
 
 		slidesField.style.transform = `translateX(-${offset}px)`;
@@ -469,7 +472,6 @@ window.addEventListener('DOMContentLoaded', () => {
 		} else {
 			slideIndex--;
 		}
-
 
 		currentSlide();
 		dotsOpacity();
@@ -481,7 +483,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			const slideTo = e.target.getAttribute('data-slide-to');
 
 			slideIndex = slideTo;
-			offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+			offset = changeString(width) * (slideTo - 1);
 
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
